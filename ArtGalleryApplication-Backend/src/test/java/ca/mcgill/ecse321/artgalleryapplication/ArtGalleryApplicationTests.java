cfae6182ca3a3c09048651fffdfe3cc838ae1ff3
Arian Omidi <arian.omidi@icloud.com>
2020-10-02 17:42:36 -0400

    Init Commit for the Backend Setup

---
+++ ArtGalleryApplication-Backend/src/test/java/ca/mcgill/ecse321/artgalleryapplication/ArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.artgalleryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
