dc3d29f626906726b408064c3126fa6292fb351b
Rodolphe Baladi <rodolphebaladi@gmail.com>
2020-10-26 22:56:39 -0400

    added ArtGalleryApplicationDto

--- ArtGalleryApplication-Backend/src/main/java/ca/mcgill/ecse321/artgalleryapplication/dto/ArtGalleryApplicationDto.java
+++ ArtGalleryApplication-Backend/src/main/java/ca/mcgill/ecse321/artgalleryapplication/dto/ArtGalleryApplicationDto.java
@@ -1,4 +1,21 @@
 package ca.mcgill.ecse321.artgalleryapplication.dto;
 
 public class ArtGalleryApplicationDto {
+
+    private int id;
+
+    public ArtGalleryApplicationDto() {
+    }
+
+    ArtGalleryApplicationDto(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
