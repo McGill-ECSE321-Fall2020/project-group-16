2a586b073d179d7fa6a18d809697b5e7f148b25b
Evan Wechsler <evan.wechsler@gmail.com>
2020-10-29 13:16:14 -0400

    Added test class for userProfile

---
+++ ArtGalleryApplication-Backend/src/test/java/ca/mcgill/ecse321/artgalleryapplication/service/TestUserProfileService.java
@@ -0,0 +1,41 @@
+package ca.mcgill.ecse321.artgalleryapplication.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.artgalleryapplication.dao.*;
+import  ca.mcgill.ecse321.artgalleryapplication.model.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TestUserProfileService {
+
+    @Mock
+    private UserRepository userDao;
+
+    @InjectMocks
+    private UserProfileService userService;
+
+    private static final String USER_KEY = "TestUser";
+
+
+
+}
