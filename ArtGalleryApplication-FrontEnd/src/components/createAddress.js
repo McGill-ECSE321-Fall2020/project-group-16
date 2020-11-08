79f208a123ee8420c44fe3aa932b7c4a088b2a7b
Arian Omidi <arian.omidi@icloud.com>
2020-11-08 17:54:59 -0600

    Create order and Address frontend

---
+++ ArtGalleryApplication-FrontEnd/src/components/createAddress.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+var config = require("../../config");
+
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
+
+export default {
+  name: "createaddress",
+  data() {
+    return {
+      addresses: [],
+
+      newAddress: {
+        streetAddress: '',
+        streetAddress2: '',
+        postalCode: '',
+        city: 'Montreal',
+        province: 'QB',
+        country: 'Canada'
+      },
+
+      errorAddress: '',
+      response: []
+    }
+  },
+  created: function () {
+    // Initializing persons from backend
+    AXIOS.get("/address")
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.addresses = response.data;
+      })
+      .catch(e => {
+        this.errorAddress = e;
+      });
+  },
+  methods: {
+
+    createAddress: function (username, artworkId) {
+      var indexUser = this.users.map(x => x.username).indexOf(username)
+      var indexArtwork = this.artworks.map(x => x.artworkId).indexOf(parseInt(artworkId))
+      var user = this.users[indexUser]
+      var artwork = this.artworks[indexArtwork]
+
+      AXIOS.post("/orders/place-order/".concat(user.username), {}, {
+        params: {
+          artworkId: artwork.artworkId
+        }
+      })
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.orders.push(response.data);
+          this.errorPlaceOrder = ''
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorPlaceOrder = errorMsg;
+        });
+    },
+    createAddress: function (streetAddress, streetAddress2, postalCode, city, province, country) {
+      AXIOS.post("/address/", {}, {
+        params: {
+          streetAddress: streetAddress,
+          streetAddress2: streetAddress2,
+          postalCode: postalCode,
+          city: city,
+          province: province,
+          country: country,
+        }
+      }
+      )
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.addresses.push(response.data);
+          this.errorAddress = "";
+          this.newAddress.streetAddress = "";
+          this.newAddress.streetAddress2 = "";
+          this.newAddress.postalCode = "";
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorAddress = errorMsg;
+        });
+    }
+  }
+};
--- ArtGalleryApplication-FrontEnd/src/router/index.js
+++ ArtGalleryApplication-FrontEnd/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import PurchaseArtwork from '@/components/PurchaseArtwork.vue'
+import CreateAddress from '@/components/CreateAddress.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: "/purchase",
+      name: "PurchaseArtwork",
+      component: PurchaseArtwork
+    },
+    {
+      path: "/create-address",
+      name: "CreateAddress",
+      component: CreateAddress
     }
   ]
 })
