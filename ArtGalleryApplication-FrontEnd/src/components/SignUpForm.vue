5c6c3825a16cda2a6534acf0a45b724a32b3c81e
Evan Wechsler <evan.wechsler@gmail.com>
2020-11-08 11:50:10 -0500

    Added funcitonality to signup and login form

--- ArtGalleryApplication-FrontEnd/src/components/SignUpForm.vue
+++ ArtGalleryApplication-FrontEnd/src/components/SignUpForm.vue
@@ -6,26 +6,66 @@
           type="text"
           name="first-name"
           id="first-name"
+          v-model="firstName"
       /></span>
       <span
         ><label for="last-name">Last Name</label><br />
-        <input type="text" name="last-name" id="last-name"
+        <input type="text" name="last-name" id="last-name" v-model="lastName"
       /></span>
     </div>
 
     <label for="username">Username</label><br />
-    <input type="text" name="username" id="username" /><br />
+    <input type="text" name="username" id="username" v-model="username" /><br />
     <label for="email">Email</label><br />
-    <input type="email" name="email" id="email" /><br />
+    <input type="email" name="email" id="email" v-model="email" /><br />
     <label for="password">Password</label><br />
-    <input type="password" name="password" id="password" /><br />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      v-model="password"
+    /><br /><br />
 
-    <div class="button"><p>Sign Up</p></div>
+    <div class="button" v-on:click="signup()">Sign Up</div>
   </div>
 </template>
 <script>
+import Axios from "axios";
 export default {
   name: "SignUpForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      user: "",
+      errorUser: "",
+    };
+  },
+  methods: {
+    signup: function signup() {
+      alert(this.username + this.email);
+      AXIOS.post("/users/".concat(username), {
+        params: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+        },
+      })
+        .then((response) => {
+          this.user = response.data;
+          console.log(this.user);
+        })
+        .catch((e) => {
+          this.errorUser = e;
+          console.log(this.errorUser);
+        });
+      alert(this.user);
+    },
+  },
 };
 </script>
 <style scoped>
@@ -41,12 +81,16 @@ input {
   justify-content: space-between;
 }
 .button {
-  align-self: center;
   padding: 5px;
   border-radius: 10px;
   background-image: linear-gradient(to right, red, orange);
   width: max-content;
   text-align: center;
   color: white;
+  margin: auto;
+  height: max-content;
+  font-size: 1.3em;
+  font-style: bold;
+  cursor: pointer;
 }
 </style>
