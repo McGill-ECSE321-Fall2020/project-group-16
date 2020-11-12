0e0d9f6b26578104ab3829c19fd68b1a56367ae4
Dina Shoham <dina.shoham@gmail.com>
2020-11-12 13:56:28 -0500

    layout stuff

--- ArtGalleryApplication-FrontEnd/src/components/ArtPage.vue
+++ ArtGalleryApplication-FrontEnd/src/components/ArtPage.vue
@@ -1,47 +1,69 @@
 <template>
     <div id="artpage">
-        <p class="title">Art Title</p>
-        <p class="artist">Artist</p>
-        <p class="date">November 11, 2020</p>
-        <p class="descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat commodo elit. Morbi vel iaculis augue. Cras egestas aliquam consectetur. Sed lacinia, nisi sit amet fringilla convallis, lectus orci venenatis nunc, id tincidunt lorem ex nec arcu. Curabitur quis sapien efficitur, vulputate tellus vel, finibus urna. </p>
+        <div class="box-1">
+            <div class="outline">
+                <img src="../assets/birth-of-venus-botticelli.jpg"> 
+            </div>
+        </div>
+        <div class="box-2">
+            <p class="title">Title</p>
+            <p class="artist">Artist</p>
+            <p class="date">November 11, 2020</p>
+            <p class="descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat commodo elit. Morbi vel iaculis augue. Cras egestas aliquam consectetur. Sed lacinia, nisi sit amet fringilla convallis, lectus orci venenatis nunc, id tincidunt lorem ex nec arcu. Curabitur quis sapien efficitur, vulputate tellus vel, finibus urna. </p>   
+            <p> <button class="purchase">$100 - PURCHASE</button> </p>
+            <p> <button class="more">MORE FROM THIS ARTIST</button> </p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-     name: "ArtPage",
-        components: {
-            ArtPage
-        },
+     name: "ArtPage"
 }
 </script>
 
 <style scoped>
-   #artpage {
-       background: #E5E5E5;
-   }
+    #artpage {
+        padding: 60px;
+        display: flex; 
+    }
+
+    .box-1 {
+        display: flex;
+        flex: 1;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .outline {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border: 1px solid #000000;
+        width: 90%;
+        height: 95%;
+    }
+
+    img {
+        width: 90%;
+    }
+
+    .box-2 {
+        text-align: left;
+        flex: 1;
+    }
 
    .title {
-        position: absolute;
-        width: 381px;
-        height: 59px;
-        left: 836px;
-        top: 218px;
-
         font-family: Montserrat;
         font-style: normal;
         font-weight: 600;
         font-size: 48px;
         line-height: 59px;
+        color: #000000;
     }
 
     .artist {
-        position: absolute;
-        width: 381px;
-        height: 59px;
-        left: 836px;
-        top: 284px;
-
         font-family: Montserrat;
         font-style: normal;
         font-weight: normal;
@@ -51,12 +73,6 @@ export default {
     }
 
     .date {
-        position: absolute;
-        width: 381px;
-        height: 29px;
-        left: 836px;
-        top: 356px;
-
         font-family: Montserrat;
         font-style: italic;
         font-weight: normal;
@@ -67,21 +83,35 @@ export default {
     }
 
     .descrip {
-        position: absolute;
-        width: 381px;
-        height: 348px;
-        left: 836px;
-        top: 398px;
-
         font-family: Montserrat;
         font-style: italic;
-        font-weight: bold;
+        font-weight: normal;
         font-size: 24px;
         line-height: 29px;
 
         color: #000000;
     }
 
+    button {
+        width: 350px;
+        height: 50px;
+        border-radius: 3px;
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 29px;
+        text-align: center;
+    }
 
+    .purchase {
+        background: #006F45;
+        color: #E9E7DB;
+    }
+
+    .more {
+        color: #000000;
+        border: 1px solid #000000;
+    }
 
 </style>
--- ArtGalleryApplication-FrontEnd/src/components/BrowseArt.vue
+++ ArtGalleryApplication-FrontEnd/src/components/BrowseArt.vue
@@ -1,11 +1,29 @@
 <template>
     <div id="browseart"> 
-        <div>
-            <h1>Browse Artwork</h1>
+        <!-- <div>
             <div v-bind:key="artwork.id" v-for="artwork in artworks">
               <ArtProduct v-bind:artwork="artwork" v-on:goto-artwork="$emit('goto-artwork', artwork.id)" />
             </div>
         </div>
+        -->
+
+        <div class="container">
+            <div class="row">
+                row 1
+            </div>
+            <div class="row">
+                <div class="col-1">
+                    row 2 col 1
+                </div>
+                <div class="col-2">
+                    row 2 col 2
+                </div>
+            </div>
+            <div class="row">
+                <button>hi</button>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -21,9 +39,40 @@ export default {
 </script>
 
 <style>
-  #browseart {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background: #f2ece8;
-  }
+    #browseart {
+        padding: 60px;
+        display: flex;
+    }
+
+    .container {
+        width: 50%;
+        height: 50%;
+        flex-direction: column;
+        border: 2px so
+        lid black;
+        padding: 10px;
+    }
+
+    .row {
+        border: 2px solid red;
+        padding: 10px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .col-1 {
+        border: 2px solid blue;
+        padding: 10px;
+        flex: 3;
+    }
+
+    .col-2 {
+        border: 2px solid blue;
+        padding: 10px;
+        flex: 1;
+
+        text-align: right;
+    }
 </style>
