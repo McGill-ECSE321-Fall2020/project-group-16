53755a2cde58fd7831eb40d6b71ab6e26f53a150
Dina Shoham <dina.shoham@gmail.com>
2020-11-11 20:42:02 -0500

    missed some stuff last commit

--- ArtGalleryApplication-FrontEnd/src/router/index.js
+++ ArtGalleryApplication-FrontEnd/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import BrowseArt from '@/components/BrowseArt'
 
 Vue.use(Router)
 
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/browseart',
+      name: 'BrowseArt',
+      component: BrowseArt
+    },
+    {
+      path: '/artPage',
+      name: 'ArtPage',
+      component: ArtPage
     }
   ]
 })
