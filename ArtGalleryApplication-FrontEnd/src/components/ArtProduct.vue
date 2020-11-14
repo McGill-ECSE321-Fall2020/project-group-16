eb4549a138b5c02fd0ae9ed49caf1070cebe6a53
dina-shoham <dina.shoham@gmail.com>
2020-11-14 17:03:57 -0500

    last thing didn't work idk

--- ArtGalleryApplication-FrontEnd/src/components/ArtProduct.vue
+++ ArtGalleryApplication-FrontEnd/src/components/ArtProduct.vue
@@ -17,7 +17,7 @@
         <div class="for-sale">${{price}}</div>
       </div>
       <div class="row-1">
-        <button @click="$router.push({ path: `/artworks/${artworkId}` }), router.params">VIEW ARTWORK</button>
+        <button @click="$router.push({ path: `/artworks/${artworkId}` })">VIEW ARTWORK</button>
       </div>
     </div>
   </div>
