f55d74716caf0b4b29f45eadc9466f17c3ffcc59
Evan Wechsler <evan.wechsler@gmail.com>
2020-11-14 17:39:18 -0500

    Fixed view artwork bug

--- ArtGalleryApplication-FrontEnd/src/js/viewArtwork.js
+++ ArtGalleryApplication-FrontEnd/src/js/viewArtwork.js
@@ -1,35 +1,35 @@
-import AXIOS from "./axiosInstance.js";
+import { AXIOS } from "./axiosInstance.js";
 
 export default {
-  name: "ViewArtwork",
-  data() {
-    return {
-      artwork: {
-        artworkId: "",
-        title: "",
-        description: "",
-        dimensions: "",
-        medium: "",
-        collection: "",
-        creationDate: "",
-        price: "",
-        imageUrl: "",
-        status: "ForSale"
-      }
-    };
-  },
-  created: function() {
-    this.artwork.artworkId = this.$route.params;
-    console.log(artworkId);
-  },
-  mounted: function() {
-    AXIOS.get(`/artworks/${this.artwork.artworkId}`)
-      .then(response => {
-        this.artwork = response.data[0];
-        console.log(this.artwork);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
+    name: "ViewArtwork",
+    data() {
+        return {
+            artwork: {
+                artworkId: "",
+                title: "",
+                description: "",
+                dimensions: "",
+                medium: "",
+                collection: "",
+                creationDate: "",
+                price: "",
+                imageUrl: "",
+                status: "ForSale"
+            }
+        };
+    },
+    created: function() {
+        this.artwork.artworkId = this.$route.params.artworkId;
+        console.log(artworkId);
+    },
+    mounted: function() {
+        AXIOS.get(`/artworks/${this.artwork.artworkId}`)
+            .then(response => {
+                this.artwork = response.data;
+                console.log(this.artwork);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
 };
