16ec874ca3784839cee24b839f89632bfa32e685
Rodolphe Baladi <rodolphebaladi@gmail.com>
2020-11-21 22:46:23 -0500

    completed basic setup + register implemented for test

--- ArtGalleryApplication-Android/app/src/main/java/ca/mcgill/ecse321/artgalleryapplication/MainActivity.java
+++ ArtGalleryApplication-Android/app/src/main/java/ca/mcgill/ecse321/artgalleryapplication/MainActivity.java
@@ -1,19 +1,26 @@
 package ca.mcgill.ecse321.artgalleryapplication;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        refreshErrorMessage();
     }
 
     @Override
@@ -52,5 +60,63 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * SignUp method
+     * @param v
+     */
+    public void signUp(View v) {
+        error = "";
+
+        final TextView firstName = (TextView) findViewById(R.id.user_firstName);
+        final TextView lastName = (TextView) findViewById(R.id.user_lastName);
+        final TextView email = (TextView) findViewById(R.id.user_email);
+        final TextView username = (TextView) findViewById(R.id.user_username);
+        final TextView password = (TextView) findViewById(R.id.user_password);
+
+        RequestParams params = new RequestParams();
+        params.put("email", email.getText());
+        params.put("firstName", firstName.getText());
+        params.put("lastName", lastName.getText());
+        params.put("password", password.getText());
+
+        HttpUtils.post("users/" + username.getText().toString(), params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+
+                firstName.setText("");
+                lastName.setText("");
+                email.setText("");
+                username.setText("");
+                password.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    /**
+     * Helper method for error handling
+     * Displays error message on the screen, if there is any
+     */
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
     }
 }
